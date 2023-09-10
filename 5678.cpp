ed3e40f8e3f93d2aeb534368058d273ef5f25760
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-09-10 17:41:47 +0300

    Update 5678.cpp

--- 5678.cpp
+++ 5678.cpp
@@ -17,14 +17,14 @@ int itc_max(int num1, int num2)
     {
         return num1;
     }
-    return nim2;
+    return num2;
 }
 
-int itc_min(int num, int num2)
+int itc_min(int num1, int num2)
 {
-    if(num<num2)
+    if(num1<num2)
     {
-        return num;
+        return num1;
     }
     return num2;
 }
