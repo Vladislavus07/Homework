11fea6cd64fa01b9269738d872d5b5e027a6102f
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-09-10 17:40:52 +0300

    Update 1234.cpp

--- 1234.cpp
+++ 1234.cpp
@@ -9,14 +9,14 @@ void itc_name()
 
 void itc_fio()
 {
-    cout << "Dragunsky Vladislav Maksimovich"
+    cout << "Dragunsky Vladislav Maksimovich";
 }
 
 int itc_abs(int num)
 {
     if (num<0)
         {
-            num=num*-1
+            num=num*-1;
         }
     return num;
 }
@@ -25,12 +25,12 @@ double itc_fabs(int num)
 {
     if (num<0)
         {
-            num=num*-1
+            num=num*-1;
         }
     return num;
 }
 
-int itc_revnbr(int num)
+int itc_revnbr(int num5)
 {
-    return (num-num/10)*100 + (num/10-num/100*10)*10 + (num/100);
+    return (num5-num5/10*10)*100 + (num5/10-num5/100*10)*10 + (num5/100);
 }
