304e8cbd610aae974cf2474bcafb7504ee017982
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-09-11 10:35:20 +0300

    Update 3456.cpp

--- 3456.cpp
+++ 3456.cpp
@@ -16,7 +16,7 @@ double itc_pow(int num, int step)
     double temp = double(num);
     if(step>0)
     {
-        for (int i = 0; i<step; i++)
+        for (int i = 1; i<step; i++)
         {
             temp=temp*double(num);
         }
@@ -28,13 +28,14 @@ double itc_pow(int num, int step)
     }
     else
     {
-        for (int i = 0; i<step; i++)
+        for (int i = -1; i>step; i--)
         {
             temp=temp*double(num);
         }
         return 1/temp;
     }
 }
+
 bool itc_inpositive(int num)
 {
     if(num>=0)
